e9f9329c8c56a1318f9908688703cc1158e0ebb0
Steve Purkis <spurkis@cpan.org>
2013-01-19 16:33:49 -0500

    bumped up version for release

--- lib/App/Prove/Plugin/HTML.pm
+++ lib/App/Prove/Plugin/HTML.pm
@@ -18,7 +18,7 @@ use warnings;
 
 use TAP::Formatter::HTML;
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 sub import {
     my ($class, @args) = @_;
--- lib/TAP/Formatter/HTML.pm
+++ lib/TAP/Formatter/HTML.pm
@@ -95,7 +95,7 @@ use constant severity_map => {
 			      5 => 'very-high',
 			     };
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 our $FAKE_WIN32_URIS = 0; # for testing only
 
 sub _initialize {
